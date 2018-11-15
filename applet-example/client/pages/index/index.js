097edeac78d100d20a80debf76b00ce96fd6c7a1
xiaomajia <1134126206@qq.com>
2018-11-15 18:22:34 +0800

    applets-configuration

---
+++ applet-example/client/pages/index/index.js
@@ -0,0 +1,28 @@
+/*<remove trigger="prod">*/
+import {
+  test,
+  test2
+} from '../../lib/api-mock'
+/*</remove>*/
+
+/*<jdists trigger="prod">
+import {test, test2} from '../../lib/api'
+</jdists>*/
+
+//index.js
+//获取应用实例
+const app = getApp()
+
+var id = `"HBmkPItFU9ZCkNoFBqCfK9BuJLeDc5khkkOSY+KRBkPlX+UWifSKaYEyaQ84h6gU1XJNmnR9lHjzgHzKspmVQn7f0jCzMpHVdOL0DrKLJBJ3iUMgys+LTEoQ+q88bmI7ZA1CBFdSe33XRU6aDXbFChVhxx3CFNw3oqo7tof6yLroIWLz0jrhCiGUkEBULAPBVjW2w+4u8Pjjjss="`
+
+Page({
+  data: {
+    today: {
+      icon: '24xiaoshitianqi'
+    }
+  },
+
+  onLoad: function () {
+    
+  }
+})
