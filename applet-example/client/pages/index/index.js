9c7ca9db22119cbd0115dd4ac88197fd93983552
xiaomajia <1134126206@qq.com>
2018-11-16 18:13:14 +0800

    weather-index

--- applet-example/client/pages/index/index.js
+++ applet-example/client/pages/index/index.js
@@ -1,28 +1,121 @@
 /*<remove trigger="prod">*/
-import {
-  test,
-  test2
-} from '../../lib/api-mock'
+
 /*</remove>*/
 
 /*<jdists trigger="prod">
-import {test, test2} from '../../lib/api'
+
 </jdists>*/
 
 //index.js
 //获取应用实例
 const app = getApp()
 
-var id = `"HBmkPItFU9ZCkNoFBqCfK9BuJLeDc5khkkOSY+KRBkPlX+UWifSKaYEyaQ84h6gU1XJNmnR9lHjzgHzKspmVQn7f0jCzMpHVdOL0DrKLJBJ3iUMgys+LTEoQ+q88bmI7ZA1CBFdSe33XRU6aDXbFChVhxx3CFNw3oqo7tof6yLroIWLz0jrhCiGUkEBULAPBVjW2w+4u8Pjjjss="`
-
 Page({
   data: {
+    bgColor: '#5F7A95',
+    bgImage: '',
+    // 实时天气
+    current: {
+      icon: 'yin',
+      temp: '16',
+      weather: '阴',
+      wet: '79%',
+      wind: '西北风 3级',
+      tips: '现在的温度比较凉爽哦~'
+    },
+    // 空气质量
+    air: {
+      qualityColor: '#1BBA99',
+      quality: '优',
+      score: '33'
+    },
+    // 今日天气
     today: {
-      icon: '24xiaoshitianqi'
-    }
+      temperature: '13/16°',
+      icon: 'yin',
+      weather: '阴'
+    },
+    // 明日天气
+    tomorrow: {
+      temperature: '12/16°',
+      icon: 'xiaoyu',
+      weather: '小雨'
+    },
+    // 24小时天气
+    hourlyData: [{
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }, {
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }, {
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }, {
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }, {
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }, {
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }, {
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }, {
+      time: '8:00',
+      icon: 'yin',
+      temp: '11'
+    }],
+    // 每周天气
+    weeklyData: [{
+      day: '今天',
+      temp: '11/15',
+      weather: '阴',
+      icon: 'yin'
+    }, {
+      day: '明天',
+      temp: '11/15',
+      weather: '阴',
+      icon: 'yin'
+    }, {
+      day: '后天',
+      temp: '11/15',
+      weather: '阴',
+      icon: 'yin'
+    }, {
+      day: '周一',
+      temp: '11/15',
+      weather: '阴',
+      icon: 'yin'
+    }, {
+      day: '周二',
+      temp: '11/15',
+      weather: '阴',
+      icon: 'yin'
+    }, {
+      day: '周三',
+      temp: '11/15',
+      weather: '阴',
+      icon: 'yin'
+    }, {
+      day: '周四',
+      temp: '11/15',
+      weather: '阴',
+      icon: 'yin'
+    }],
+    
   },
 
   onLoad: function () {
-    
+
   }
 })
